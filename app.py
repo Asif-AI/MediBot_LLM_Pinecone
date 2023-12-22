7fb08839602f365a0b51df5f30fd0d7acdf53e7d
asif <aasifkhan02@gmail.com>
2023-12-23 00:52:19 +0530

    app.py updated

---
+++ app.py
@@ -0,0 +1,59 @@
+
+from flask import Flask, render_template, request, jsonify
+from src.utils import download_hugging_face_embeddings
+from langchain.vectorstores import Pinecone
+import pinecone
+from langchain.prompts import PromptTemplate
+from langchain.llms import CTransformers
+from langchain.chains import RetrievalQA
+
+from dotenv import load_dotenv
+from src.prompt import *
+import os
+
+app = Flask(__name__)
+
+load_dotenv()
+PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY')
+PINECONE_API_ENV = os.environ.get('PINECONE_API_ENV')
+
+#download embeddings
+embeddings = download_hugging_face_embeddings()
+
+#Initializing the Pinecone
+pinecone.init(      
+	api_key='c3701bf9-f5f8-4b3d-82da-118a22b743bf',      
+	environment='gcp-starter')      
+index_name = 'medibot'
+
+#using existing index
+docsearch = Pinecone.from_existing_index(index_name, embeddings)
+
+#Create Prompt Template
+PROMPT=PromptTemplate(template=prompt_template, input_variables=["context", "question"])
+chain_type_kwargs={"prompt": PROMPT}
+
+#initialize 
+llm = CTransformers(model = "model/llama-2-7b-chat.ggmlv3.q4_0.bin",
+                        model_type = 'llama',
+                        config = {'max_new_tokens': 512,
+                                  "temperature" : 0.8})
+
+
+
+RetrievalQA.from_chain_type(
+    LLM=llm,
+    chain_type = "stuff",
+    retriever = docsearch.as_retriever(search_kwargs={'K': 2,}),
+    return_source_documents = True,
+    chain_type_kwargs= chain_type_kwargs)
+
+#Make a default route for flask
+@app.route("/")
+def index():
+    return render_template('chat.html')
+
+
+#initialize flask
+if __name__ == "__main__":
+    app.run(host = '0.0.0.0', port = 8080, debug=True)
---
+++ src/prompt.py
@@ -0,0 +1,12 @@
+
+
+prompt_template = """
+Use the following piece of  information to answer the user's questions.
+If you do not know that answer, just say that I do not know, don't try to make up the answers.
+
+Context = {context}
+Question = {question}
+
+Only return the helpful answer below and nothing else:
+Helpful answer:
+"""
